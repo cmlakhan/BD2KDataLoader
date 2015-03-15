231c2f708843d5858696be620a0e8d6a4d124207
Chirag Lakhani <chirag.lakhani@gmail.com>
2015-03-15 16:49:55 -0400

    update

--- census.py
+++ census.py
@@ -1,15 +1,27 @@
 from census import Census
 from us import states
+import xml.etree.cElementTree as ET
+import urllib2
 
 
+tree = ET.parse(urllib2.urlopen('http://api.census.gov/data/2013/acs5/variables.xml'))
+root = tree.getroot()
+
+print root.tag
+
+for child_of_root in root[0]:
+	print child_child_of_root.attrib
 
 
 
 c = Census("0ce452c10608a1ce5cbc4c43d1d2520323845086")
 
 
+
+
+
 x=c.acs.get('NAME,B01001_004E', {'for': 'zip code tabulation area:*'},year=2013)
 
-
+x=c.acs.get('NAME,B01001_004E', {'for': 'zip code tabulation area:*'},year=2013)
 
 
